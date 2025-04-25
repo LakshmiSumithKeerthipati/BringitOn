79fed8cf20df2f80fb74e4ab2a00d4754bc1a4d9
LakshmiSumithKeerthipati <sumithkeerthipati@gmail.com>
2025-04-25 15:47:37 -0500

    Add files via upload

---
+++ Simple Program.py
@@ -0,0 +1,15 @@
+print("hello world")
+a= int(input('enter the first number: '))
+b= int(input('enter the second number: '))
+print('Addition= ', a+b)
+print('Subtraction= ',a-b)
+print('Multiplication= ',a*b)
+print('Division= ',a/b)
+...
+...
+...
+c= (input('enter your first name: '))
+d= (input('enter your last name: '))
+print(f"Hello, {c} {d} welcome! to the python program.")
+
+
