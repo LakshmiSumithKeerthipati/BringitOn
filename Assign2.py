d3d5a86539a8274f60c1996e46cb6631c2173c98
LakshmiSumithKeerthipati <sumithkeerthipati@gmail.com>
2025-04-28 12:13:21 -0500

    Add files via upload
    if else and Loop

---
+++ Assign2.py
@@ -0,0 +1,23 @@
+
+#1
+a= int(input('enter a number: '))
+
+if (a%2==0):
+    print(f' {a} is an even number')
+else:
+   print(f' {a} is an odd number')
+
+#2
+n=0
+
+for i in range(1,51):
+    n += i
+
+print (f'The sum of the numbers from 1 to 50 is {n}')
+
+
+
+
+
+
+
